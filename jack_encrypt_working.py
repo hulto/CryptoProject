3d89b3153b834f670fcad61e8471a01f58462667
hulto <jack.m.mckenna@gmail.com>
2018-04-30 23:53:20 -0400

    done

--- jack_encrypt_working.py
+++ jack_encrypt_working.py
@@ -1,8 +1,13 @@
+import sys
+import numpy as np
+
 def encrypt(inString, inPassword):
-	prime = 127
+	prime = 257
 	n = len(inString)
-	p = [ord(i) for i in inString]
-	t = [ord(i) for i in inPassword]
+	p = inString
+	t = inPassword
+	#p = [ord(i) for i in inString]
+	#t = [ord(i) for i in inPassword]
 	r = 0
 
 
@@ -40,10 +45,12 @@ def encrypt(inString, inPassword):
 
 
 def decrypt(inString, inPassword):
-	prime = 127
+	prime = 257
 	n = len(inString)
-	c = [ord(i) for i in inString]
-	t = [ord(i) for i in inPassword]
+	c = inString
+	t = inPassword
+	#c = [ord(i) for i in inString]
+	#t = [ord(i) for i in inPassword]
 	r = 0
 	l = [None] * (n)
 	p = [None] * (n)
@@ -87,11 +94,127 @@ def decrypt(inString, inPassword):
 	return p
 
 
+def genMatrix(size):
+	matrix = np.empty((size,size))
+	for i in range(0, size):
+		for j in range(0, size):
+			val = 0
+			if i == j:
+				val = 1
+			elif i > j:
+				if i % 2 == 0:
+					if j % 2 == 0:
+						val = 0
+					else:
+						val = 0
+				else:
+					if j % 2 == 0:
+						val = 0
+					else:
+						val = 0
+			else:
+				val = 0
+			matrix[i][j] = val
+	#print matrix
+	return matrix
 
-a = encrypt("abcdefg\n", "PASSWORD")
+
+def enc(plaintext, password):
+	ct = []
+	matrix = genMatrix(len(plaintext))
+	plaintext = np.matmul(plaintext, matrix)
+
+	ct = encrypt(plaintext, password)
+
+	inverse = np.linalg.inv(matrix)
+	ct = np.matmul(ct, matrix)
+
+	return ct
+
+
+def dec(ciphertext, password):
+	pt = []
+	matrix = genMatrix(len(ciphertext))
+	ciphertext = np.matmul(ciphertext, matrix)
+
+	pt = decrypt(ciphertext, password)
+
+	inverse = np.linalg.inv(matrix)
+	pt = np.matmul(pt, matrix)
+
+	return pt
+
+
+
+def main():
+	'''
+	#Will take in bytes from a file.
+	pt_d = [1,2,3,4,5,6,7,8,8,9,10]
+	passw_d = [1,2,3,1,2,3]
+
+
+	#Reading in a file seems like its going to take longer than I have. :(
+
+	print pt_d
+	a = enc(pt_d, passw_d)
+	print a
+	b = dec(a, passw_d)
+	print b
+	'''
+
+	f = open('file', 'r')
+	a = np.fromfile(f, dtype=np.uint8)
+
+	
+	pt_d = a
+	passw_d = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+
+	#Reading in a file seems like its going to take longer than I have. :(
+
+	#print pt_d
+	a = enc(pt_d, passw_d)
+	print a
+	b = dec(a, passw_d)
+	#print b
+
+
+
+
+
+
+	sys.exit()
+	with open('file', 'rb') as f:
+		array = []
+		while True:
+			a = f.read(1)
+			if a:
+				array.append(a)
+			else:
+				f.close()
+				print array
+				break
+
+
+
+
+main()
+
+
+sys.exit()
+
+
+pt_a = "abcdefg\n"
+passw_a = "1234"
+
+pt_a = [ord(i) for i in pt_a]
+passw_a = [ord(i) for i in passw_a]
+
+
+print pt_a
+a = encrypt(pt_a, passw_a)
 print a
-b =  [chr(i) for i in a]
-c = decrypt(b,"PASSWORD")
+c = decrypt(a,passw_a)
 print c
 d = [chr(i) for i in c]
 print d
